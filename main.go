d6c92070e832b00d9024b083330364008e72f4db
Fernando Crelick <fccrelick@protonmail.com>
2023-07-03 19:56:35 -0300

    added mongodb connection

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/Fccrelick/crudGO/src/configuration/database/mongodb"
 	"github.com/Fccrelick/crudGO/src/controller"
 	"github.com/Fccrelick/crudGO/src/controller/routes"
 	"github.com/Fccrelick/crudGO/src/model/service"
@@ -16,13 +17,13 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	mongodb.InitConnection()
+
 	service := service.NewUserDomainService()
 	userController := controller.NewUserControllerInterface(service)
 
 	router := gin.Default()
-	log.Println("got here")
 	routes.InitRoutes(&router.RouterGroup, userController)
-	log.Println("got here")
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
